d104ab15491794798bd0a32c105b45a556d9ac0e
Urnark <christoffer5511@gmail.com>
2020-02-24 16:26:04 +0100

    Code for looking for memory leaks

--- Twice/src/main.cpp
+++ Twice/src/main.cpp
@@ -5,6 +5,8 @@
 extern FILE* yyin;
 extern twice::Loc* curLoc;
 
+extern int yylex_destroy(void);
+
 void yy::parser::error(const yy::location&loc, std::string const& err)
 {
 	std::cout << loc.begin.filename->c_str() << "(" << loc.begin.line << "," << loc.begin.column << "): " << "ERROR: " << err << std::endl;
@@ -12,6 +14,8 @@ void yy::parser::error(const yy::location&loc, std::string const& err)
 
 int main(int argc, char** argv)
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	std::string filename = "test.twi";
 	std::string scriptsFolder = "scripts/";
 	if (fopen_s(&yyin, (scriptsFolder + filename).c_str(), "r") != 0)
@@ -27,5 +31,6 @@ int main(int argc, char** argv)
 		fclose(yyin);
 	delete curLoc;
 
+	yylex_destroy();
 	return 0;
 }
